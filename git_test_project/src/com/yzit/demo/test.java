368c856b86fb260104603128beaa559493563ec9
admin <admin@itcast.cn>
2020-06-27 21:25:43 +0800

    向本地工程提交代码

---
+++ git_test_project/src/com/yzit/demo/test.java
@@ -0,0 +1,10 @@
+package com.yzit.demo;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
